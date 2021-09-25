a6dd89f5394974ffed783e9db27059bb7a13a16b
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2021-09-25 17:39:19 +0530

    Update 14. Duplicate Elements.c

--- #02 - Arrays/14. Duplicate Elements.c
+++ #02 - Arrays/14. Duplicate Elements.c
@@ -26,17 +26,6 @@ int max(struct Array arr)
     return m;
 }
 
-int min(struct Array arr)
-{
-    int m = arr.A[0];
-    for (int i = 1; i < arr.length; i++)
-    {
-        if (arr.A[i] < m)
-            m = arr.A[i];
-    }
-    return m;
-}
-
 void sorted_duplicate(struct Array arr)
 {
     for (int i = 0; i < arr.length-1; i++)
