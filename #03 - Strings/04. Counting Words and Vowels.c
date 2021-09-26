eb1a8ad884e0fcea9d73d6ff58e7395592b07572
aryanblouria <aryanblouria@gmail.com>
2021-09-26 14:12:06 +0530

    Create 04. Counting Words and Vowels.c

---
+++ #03 - Strings/04. Counting Words and Vowels.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[] = "My name is Aryan   Blouria";
+    int words = 1, vowels = 0, consonants = 0, i = 0;
+
+    while (s[i] != '\0')
+    {
+        if (s[i] == ' ')
+        {
+            if (s[i-1] != ' ')
+                words++;
+        }
+        else if (s[i] == 'A' || s[i] == 'a' || s[i] == 'E' || s[i] == 'e' || s[i] == 'I' || s[i] == 'i' || s[i] == 'O' || s[i] == 'o' || s[i] == 'U' || s[i] == 'u')
+            vowels++;
+        else if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+            consonants++;
+        i++;
+    }
+
+    printf("No. of words: %d\n", words);
+    printf("No. of vowels: %d\n", vowels);
+    printf("No. of consonants: %d\n", consonants);
+}
