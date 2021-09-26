b60b3731b0ec459c4ee8a1408c22491aa74604db
aryanblouria <aryanblouria@gmail.com>
2021-09-26 18:24:08 +0530

    Create 09. Duplicates in String.c

---
+++ #03 - Strings/09. Duplicates in String.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int max(char* s, int length)
+{
+    int m = s[0];
+    for (int i = 1; i < length; i++)
+    {
+        if (s[i] > m)
+            m = s[i];
+    }
+    return m;
+}
+
+void duplicate1(char s[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        int count = 0;
+        for(int j = 0; j < length; j++)
+            if (s[i] == s[j])
+                count++;
+
+        if (count > 1)
+            printf("%c has %d duplicates.\n", s[i], count-1);
+        else
+            printf("%c has no duplicates.\n", s[i]);
+    }
+}
+
+void duplicate2(char s[], int length)
+{
+    int hashlength = max(s, length)-97;
+    int *t = (int*)malloc(hashlength*sizeof(int));
+    for (int i = 0; i <= hashlength; i++)
+        t[i] = 0;
+    for (int i = 0; i < length; i++)
+    {
+        int temp = s[i] - 97;
+        t[temp]++;
+    }
+    for (int i = 0; i <= hashlength; i++)
+        if (t[i] > 0)
+            printf("Count of %c is %d\n", i+97, t[i]);
+}
+
+int main()
+{
+    char s[] = "abracadabra";
+    int l = 0;
+    while (s[l] != '\0')
+        l++;
+
+    int length = l;
+    duplicate1(s, length);
+    duplicate2(s, length);
+
+}
