1c65b9cb0ebf86b2cd3e26f942abbbf260e283e1
aryanblouria <aryanblouria@gmail.com>
2021-10-03 15:43:33 +0530

    Create 04. Lower Triangular Matrix Class.cpp

---
+++ #04 - Matrices/04. Lower Triangular Matrix Class.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+using namespace std;
+
+class Lower_Triangular_Matrix
+{
+private:
+    int *arr;
+    int dim;
+public:
+    Lower_Triangular_Matrix()
+    {
+        dim = 3;
+        arr = new int[dim];
+    }
+
+    Lower_Triangular_Matrix(int d)
+    {
+        dim = d;
+        arr = new int[dim];
+    }
+
+    ~Lower_Triangular_Matrix()
+    {
+        delete[] arr;
+    }
+
+    void ltm_rowmajor_display();
+    void ltm_colmajor_display();
+    void ltm_rowmajor_set(int i, int j, int a);
+    void ltm_colmajor_set(int i, int j, int a);
+    int ltm_rowmajor_get(int i, int j);
+    int ltm_colmajor_get(int i, int j);
+};
+
+void Lower_Triangular_Matrix::ltm_rowmajor_display()
+{
+    for(int i = 1; i <= dim; i++)
+    {
+        for(int j = 1; j <= dim; j++)
+        {
+            int index = (i * (i-1)/2) + j - 1;
+            if (i>=j)
+                printf("%d ", arr[index]);
+            else
+                printf("0 ");
+        }
+        printf("\n");
+    }
+}
+
+void Lower_Triangular_Matrix::ltm_rowmajor_set(int i, int j, int a)
+{
+    if (i >= j)
+    {
+        int index = (i * (i-1)/2) + j - 1;
+        arr[index] = a;
+    }
+}
+
+int Lower_Triangular_Matrix::ltm_rowmajor_get(int i, int j)
+{
+    if (i >= j)
+    {
+        int index = (i * (i-1)/2) + j - 1;
+        return arr[index];
+    }
+    return 0;
+}
+
+void Lower_Triangular_Matrix::ltm_colmajor_display()
+{
+    for(int i = 1; i <= dim; i++)
+    {
+        for(int j = 1; j <= dim; j++)
+        {
+            int index = (dim * (j-1) - (j-1)*(j-2)/2) + i - j;
+            if (i>=j)
+                printf("%d ", arr[index]);
+            else
+                printf("0 ");
+        }
+        printf("\n");
+    }
+}
+
+void Lower_Triangular_Matrix::ltm_colmajor_set(int i, int j, int a)
+{
+    if (i >= j)
+    {
+        int index = (dim * (j-1) - (j-1)*(j-2)/2) + i - j;
+        arr[index] = a;
+    }
+}
+
+int Lower_Triangular_Matrix::ltm_colmajor_get(int i, int j)
+{
+    if (i >= j)
+    {
+        int index = (dim * (j-1) - (j-1)*(j-2)/2) + i - j;
+        return arr[index];
+    }
+    return 0;
+}
+
+int main()
+{
+    Lower_Triangular_Matrix *ltm1, *ltm2;
+    int dim, choice;
+    int i, j, a;
+
+    printf("Enter dimension: ");
+    scanf("%d", &dim);
+
+    ltm1 = new Lower_Triangular_Matrix(dim);
+    ltm2 = new Lower_Triangular_Matrix(dim);
+
+    do
+    {
+       cout<<endl<<"Menu of Operations"<<endl;
+       cout<<"=================="<<endl<<endl;
+       cout<<"1. Display"<<endl;
+       cout<<"2. Set Element"<<endl;
+       cout<<"3. Get Element"<<endl;
+
+       cout<<"Enter your choice: ";
+       cin>>choice;
+
+       switch(choice)
+       {
+        case 1:
+            ltm1->ltm_rowmajor_display();
+            ltm2->ltm_colmajor_display();
+            break;
+        case 2:
+            printf("Enter row number: ");
+            scanf("%d", &i);
+            printf("Enter column number: ");
+            scanf("%d", &j);
+            printf("Enter value: ");
+            scanf("%d", &a);
+            ltm1->ltm_rowmajor_set(i, j, a);
+            ltm2->ltm_colmajor_set(i, j, a);
+            break;
+        case 3:
+            printf("Enter row number: ");
+            scanf("%d", &i);
+            printf("Enter column number: ");
+            scanf("%d", &j);
+            printf("%d\n", ltm1->ltm_rowmajor_get(i, j));
+            printf("%d\n", ltm2->ltm_colmajor_get(i, j));
+       }
+    } while (choice > 0 && choice < 4);
+}
+
