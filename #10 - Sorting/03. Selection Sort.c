fb58dc310430b3c225d2181621961043c677b014
Aryan Blouria <aryanblouria@gmail.com>
2022-01-13 02:04:49 +0530

    Create 03. Selection Sort.c

---
+++ #10 - Sorting/03. Selection Sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void swap(int* a, int* b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void insertionsort(int arr[], int n)
+{
+    int k = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        k = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[k] > arr[j])
+                k = j;
+        }
+        swap(&arr[i], &arr[k]);
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 1, 2, 9, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    insertionsort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
